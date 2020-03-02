c0c8a6a7bd173ea751d476664661b0eddf05af64
FELIPE  FELDE GIUSTI <18.00402-4@maua.br>
2020-03-02 11:27:25 -0300

    Arquivo compilado

---
+++ Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
@@ -0,0 +1,9 @@
+class MeuPrograma{
+   public static void main(String[] args) {
+   
+      // Mensagem de saida do sistema
+      System.out.println("Minha primeira aplicacao Java!!");
+      System.out.println("Olá mundo!");
+      System.out.println("Estou programando no modo Conan");
+   }
+}

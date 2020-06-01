ba3492fd7c55847fd7d7e840a043cce9116feac7
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-06-01 12:39:17 -0300

    Update Compra.java
    Implementação da classe Compra

--- Linguagens1_Projetos/Projeto_exemplo_fluxo/src/br/maua/models/Compra.java
+++ Linguagens1_Projetos/Projeto_exemplo_fluxo/src/br/maua/models/Compra.java
@@ -1,7 +1,37 @@
 package br.maua.models;
 
-public class Compra {
+import java.util.Objects;
 
+public class Compra {
+    private String nome;
+    private int numero;
+
+    public Compra(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" +
+                "nome='" + nome + '\'' +
+                ", numero=" + numero +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Compra compra = (Compra) o;
+        return numero == compra.numero &&
+                Objects.equals(nome, compra.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numero);
+    }
 }
 
 

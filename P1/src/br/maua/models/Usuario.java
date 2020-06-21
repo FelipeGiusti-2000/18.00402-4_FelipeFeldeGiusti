533dfb508916b9a42f724277e671420d6a185e58
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-06-21 19:23:52 -0300

    p1 + Usuario
    Criação do documento e modelo de classe Usuario

---
+++ P1/src/br/maua/models/Usuario.java
@@ -0,0 +1,19 @@
+package br.maua.models;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;         //Talvez a senha tenha que fazer 123456 direto
+    }
+    public void criarPedido(){
+
+    }
+    public void alterarPedido() {
+
+    }
+}

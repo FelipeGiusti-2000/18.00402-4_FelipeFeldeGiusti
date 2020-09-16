b44c7d5ac8f07f0db04d0059f55c92c6e45ab584
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-09-16 11:58:16 -0300

    Interface DAOFields + fim de PokemonCardDAO
    fim da implementacao de PokemonCardDAO, criacao de uma interface DAOFields para facilitar possiveis expansoes de software, com adicao de tabelas

--- Linguagens1_Projetos/Prova_TSub1/src/giusti/felipe/dao/PokemonCardDAO.java
+++ Linguagens1_Projetos/Prova_TSub1/src/giusti/felipe/dao/PokemonCardDAO.java
@@ -6,7 +6,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PokemonCardDAO implements DAO<PokemonCard> {
+public class PokemonCardDAO implements DAO<PokemonCard>, DAOFields {
     private Connection connection;
     private String myDBConnectionString = "jdbc:sqlite:card_data.db";
 
@@ -23,7 +23,7 @@ public class PokemonCardDAO implements DAO<PokemonCard> {
         List<PokemonCard> pokemonCards = new ArrayList<>();
         try{
             Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM pokemonCard WHERE " + condition);
+            ResultSet result = statement.executeQuery(getSelectConditionalString(getTableName()) + condition);
             while(result.next()){
                 PokemonCard pokemonCard = new PokemonCard(
                         result.getString("id"),
@@ -45,22 +45,109 @@ public class PokemonCardDAO implements DAO<PokemonCard> {
 
     @Override
     public List<PokemonCard> getAll() {
-        return null;
+        List<PokemonCard> pokemonCards = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(getSelectAllString(getTableName()));
+            while(result.next()){
+                PokemonCard pokemonCard = new PokemonCard(
+                        result.getString("id"),
+                        result.getString("imageUrl"),
+                        result.getString("name"),
+                        result.getString("rarity"),
+                        result.getString("series"),
+                        result.getString("cardSet")
+                );
+                pokemonCards.add(pokemonCard);
+            }
+            result.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return pokemonCards;
     }
 
     @Override
     public void update(PokemonCard pokemonCard) {
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(getUpdateString(getTableName()));
+            preparedStatement.setString(1,pokemonCard.getId());
+            preparedStatement.setString(2,pokemonCard.getImageUrl());
+            preparedStatement.setString(3,pokemonCard.getName());
+            preparedStatement.setString(4,pokemonCard.getRarity());
+            preparedStatement.setString(5,pokemonCard.getSeries());
+            preparedStatement.setString(6,pokemonCard.getCardSet());
+            preparedStatement.setString(7,pokemonCard.getId());
 
+            preparedStatement.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(PokemonCard pokemonCard) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement((getDeleteString(getTableName())));
+            preparedStatement.setString(1,pokemonCard.getId());
 
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void create(PokemonCard pokemonCard) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getInsertString(getTableName()));
+            preparedStatement.setString(1,pokemonCard.getId());
+            preparedStatement.setString(2,pokemonCard.getImageUrl());
+            preparedStatement.setString(3,pokemonCard.getName());
+            preparedStatement.setString(4,pokemonCard.getRarity());
+            preparedStatement.setString(5,pokemonCard.getSeries());
+            preparedStatement.setString(6,pokemonCard.getCardSet());
 
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public String getTableName() {
+        return "pokemonCard";
+    }
+
+    @Override
+    public String getDeleteString(String table) {
+        return "DELETE FROM " + table + " WHERE id = ?;";
+    }
+
+    @Override
+    public String getUpdateString(String table) {
+        return "UPDATE " + table + " SET id = ?, imageUrl = ?, name = ?, rarity = ?, series = ?, cardSet = ? WHERE id = ?;";
+    }
+
+    @Override
+    public String getInsertString(String table) {
+        return "INSERT INTO " + table + " (id, imageUrl, name, rarity, series, cardSet) VALUES (?,?,?,?,?,?);";
+    }
+
+    @Override
+    public String getSelectAllString(String table) {
+        return "SELECT * FROM " + table + ";";
+    }
+
+    @Override
+    public String getSelectConditionalString(String table) {
+        return "SELECT * FROM " + table + " WHERE ";
     }
 }
 
